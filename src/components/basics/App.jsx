0e2e6854e0d92ca7b60df697c1bb0ca775d7961a
sebastian.britos <sebastian.britos.peluffo@gmail.com>
2024-05-01 02:10:12 -0300

    carousel hecho

--- src/components/basics/App.jsx
+++ src/components/basics/App.jsx
@@ -1,13 +1,18 @@
 import "./App.css";
 import { Header } from "./Header";
+import { Carousel } from "./Carousel";
+import '@coreui/coreui/dist/css/coreui.min.css';
 
 function App() {
 
 
   return (
-    <div className="h-screen w-full p-0 m-0 bg-primario">
-      <Header />
-    </div>
+    <>
+      <div className="h-screen w-full p-0 m-0 bg-primario">
+        <Header />
+        <Carousel />
+      </div>
+    </>
   );
 }
 

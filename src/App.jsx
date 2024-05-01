42babf738e80e481050904c454fefce73e9ad288
agustin.torres <agustintorres1701@gmail.com>
2024-04-30 22:43:17 -0300

    set up inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
-import { useState } from 'react'
-import { RangeCalendar } from '@nextui-org/react'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-     <RangeCalendar aria-label='Date (No Selection)' />
-     
-
-
-
-    </>
-  )
+    <div className="h-screen w-full p-0 m-0 bg-primario">
+      <div className="text-3xl font-bold ">
+        
+        <h2 className="text-8xl text-accent">Hola</h2>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,44 @@ const {nextui} = require("@nextui-org/react");
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      mobile: "375px",
+      tablet: "768px",
+      desktop: "1024px",
+    },
+    fontSize: {
+      xs: "0.75rem",
+      sm: "0.875rem",
+      base: "1rem",
+      lg: "1.125rem",
+      xl: "1.25rem",
+      "2xl": "1.5rem",
+      "3xl": "1.875rem",
+      "4xl": "2.25rem",
+      "5xl": "3rem",
+      "6xl": "4rem",
+      "7xl": "5rem",
+      "8xl": "6rem",
+      "9xl": "7rem",
+    },
+    extend: {
+      colors: {
+        primary: '#333333',
+        success: '#c7cb07',
+        default: '#e0e0e0',
+        secondary: '#5c5c5c',
+        danger: '#606c00',
+        primario: '#333333',
+        'primario-claro': '#5c5c5c',
+        'primario-claro-xl': '#b9b9b9',
+        'primario-claro-lg': '#404040',
+        'accent': '#c7cb07',
+        'accent-oscuro': '#606c00',
+        'fuente': '#e0e0e0',
+        'fondo': '#1a1a1a',
+        'fondo-claro': '#292929',
+      },
+    }
   },darkMode: "class",
   plugins: [nextui()],
 };

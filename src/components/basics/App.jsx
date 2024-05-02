acb516bb44a7862185bd80e7166449b0846c787d
agustin.torres <agustintorres1701@gmail.com>
2024-05-01 21:12:27 -0300

    principal slider carousel home done

--- src/components/basics/App.jsx
+++ src/components/basics/App.jsx
@@ -1,13 +1,22 @@
 import "./App.css";
 import { Header } from "./Header";
-
+import { Carousel } from "./Carousel";
+import { Image } from "@nextui-org/react";
 function App() {
-
+  const images = [
+    "https://4kwallpapers.com/images/wallpapers/avatar-the-last-3840x2160-13576.jpg",
+    "https://images8.alphacoders.com/133/1335152.jpg",
+    "https://wallpapercave.com/wp/wp10578910.jpg",
+  ];
 
   return (
     <>
-      <div className="h-screen w-full p-0 m-0 bg-primario">
-        <Header />
+      <div className="w-full p-0 m-0 bg-primario flex items-center flex-col">
+        <div className="h-screen">
+          <Header />
+          <Carousel className="h-[calc(100vh-620px)]" slides={images} />
+        </div>
+        <div className="h-[400px]">Hola</div>
       </div>
     </>
   );
--- src/components/basics/Header.jsx
+++ src/components/basics/Header.jsx
@@ -4,11 +4,26 @@ import "./Header.css";
 import { RiMovie2Fill } from "react-icons/ri";
 import { FaSearch, FaHome, FaSearchPlus } from "react-icons/fa";
 import { Input } from "@nextui-org/react";
+import {useState, useEffect} from "react";
 import { FaList } from "react-icons/fa6";
 export const Header = () => {
+  const [opacity, setOpacity] = useState(1);
+  useEffect(() => {
+    const handleScroll = () => {
+      let opacity = 1 - window.scrollY / 300;
+      if (opacity < 0.25) opacity = 0.25;
+      setOpacity(opacity);
+    };
+  
+    window.addEventListener("scroll", handleScroll);
+  
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
    
-    <div className="w-full bg-fondo py-2 flex items-center px-2 md:px-12 justify-between sticky top-0 z-50">
+    <div className="header-blur w-full bg-fondo h-14 py-2 flex items-center px-2 md:px-12 justify-between sticky top-0 z-50" style={{ backgroundColor: `rgba(0, 0, 0, ${opacity})` }}>
       <div className="flex items-center md:gap-1 md:text-xl text-lg font-bold text-accent">
         <RiMovie2Fill />
         <span>QuePinta</span>
@@ -16,25 +31,25 @@ export const Header = () => {
       <div className="w-full items-center text-fuente gap-5 bg-primario/80 flex justify-center text-xl fixed bottom-0 left-0 right-0 py-3 z-50 animated-opacity sm:static sm:bg-transparent">
         <a className="hover:text-accent transition-all ease-in no-underline text-fuente" href="">
         <FaHome className="sm:hidden"/>
-        <span className="text-sm text-accent hidden sm:block">Inicio</span>
+        <span className="text-sm text-fuente hover:text-accent hidden sm:block">Inicio</span>
         </a>
-        <a className="hover:text-accent transition-all ease-in no-underline text-fuente" href="">
+        <a className="hover:text-accent  transition-all ease-in no-underline text-fuente" href="">
         <FaList className="sm:hidden"/>
-        <span className="text-sm text-accent hidden sm:block">Mi Lista</span>
+        <span className="text-sm text-fuente hover:text-accent hidden sm:block">Mi Lista</span>
         </a>
         <a className="hover:text-accent transition-all ease-in no-underline text-fuente" href="">
         <FaSearchPlus className="sm:hidden"/>
-        <span className="text-sm text-accent hidden sm:block">Busqueda Avanzada</span>
+        <span className="text-sm text-fuente hover:text-accent hidden sm:block">Busqueda Avanzada</span>
         </a>
       </div>
-      <div className="flex items-center max-w-48">
+      <div className="flex items-center max-w-72 min-w-44 lg:min-w-56">
         <Input
           type="text"
           placeholder="Busca tu pelicula favorita"
           labelPlacement="outside"
           endContent={<FaSearch className="" />}
           classNames={{
-            input: "bg-transparent text-fuente ",
+            input: "bg-transparent text-fuente w-full",
             innerWrapper: "bg-transparent text-fuente hover:text-primario focus-within:text-primario",
             inputWrapper: [
               "shadow-xl",

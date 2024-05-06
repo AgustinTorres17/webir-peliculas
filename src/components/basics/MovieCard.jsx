b11b9034d675eb3d2e9ec2dd3a4225a37603a720
agustin.torres <agustintorres1701@gmail.com>
2024-05-05 22:02:16 -0300

    link a movie

--- src/components/basics/MovieCard.jsx
+++ src/components/basics/MovieCard.jsx
@@ -1,20 +1,33 @@
-import React from 'react';
+import React from "react";
 import { Button } from "@/components/ui/button";
 import { FaPlusCircle } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
-export const MovieCard = ({title, genre, year, imageUrl }) => {
-    return (
-        <div className="relative overflow-hidden aspect-video">
-            <img className="object-cover h-full w-full transform transition duration-500 ease-in-out hover:scale-110 active:scale-110" src={imageUrl} alt={title} />
-            <div className="absolute inset-0 text-center text-pretty bg-black bg-opacity-60 flex flex-col items-center justify-between md:justify-center md:p-4 md:gap-1 opacity-0 hover:opacity-100 active:opacity-100 transition-opacity duration-500">
-                <div className='md:hidden'></div>
-                <h2 className="text-sm md:text-lg font-semibold text-white leading-6">{title} ({year})</h2>
-                <p className="md:text-sm text-white text-opacity-50 hidden md:block">{genre}</p>
-                <div className="w-full pb-1 flex  justify-center">
-                    <Button variant="default">Ver Detalles</Button>
-                    <Button variant="favedMovie" className="ml-5 hidden md:block"><FaPlusCircle></FaPlusCircle></Button>
-                </div>
-            </div>
+export const MovieCard = ({ title, genre, year, imageUrl }) => {
+  return (
+    <div className="relative overflow-hidden aspect-video">
+      <img
+        className="object-cover h-full w-full transform transition duration-500 ease-in-out hover:scale-110 active:scale-110"
+        src={imageUrl}
+        alt={title}
+      />
+      <div className="absolute inset-0 text-center text-pretty bg-black bg-opacity-60 flex flex-col items-center justify-between md:justify-center md:p-4 md:gap-1 opacity-0 hover:opacity-100 active:opacity-100 transition-opacity duration-500">
+        <div className="md:hidden"></div>
+        <h2 className="text-sm md:text-lg font-semibold text-white leading-6">
+          {title} ({year})
+        </h2>
+        <p className="md:text-sm text-white text-opacity-50 hidden md:block">
+          {genre}
+        </p>
+        <div className="w-full pb-1 flex  justify-center">
+          <Link to="/movie">
+            <Button variant="default">Ver Detalles</Button>
+          </Link>
+          <Button variant="favedMovie" className="ml-5 hidden md:block">
+            <FaPlusCircle></FaPlusCircle>
+          </Button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };

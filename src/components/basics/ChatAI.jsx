b495f1db2b6ae2819c31ca2b534d3d63cef86c73
agustin.torres <agustintorres1701@gmail.com>
2024-05-18 01:09:24 -0300

    probando

--- src/components/basics/ChatAI.jsx
+++ src/components/basics/ChatAI.jsx
@@ -31,21 +31,21 @@ export const ChatAI = () => {
               <Avatar className="" isChat={true}/>
             </div>
             <div className="flex w-full justify-between z-[2]">
-              <h2 className="text-2xl font-bold tracking-wide">
+              <h2 className="text-lg md:text-2xl font-bold tracking-wide">
                 Asesorate con Chaplin
               </h2>
               <button
                 onClick={showChatHandler}
-                className="text-2xl font-bold text-fuente hover:text-accent"
+                className="text-lg md:text-2xl font-bold text-fuente hover:text-accent"
               >
                 <IoIosCloseCircle />
               </button>
             </div>
-            <p className="mt-4 text-lg font-semibold p-2 text-fuente tracking-wide">
+            <p className="mt-4 text-base md:text-lg font-semibold p-2 text-fuente tracking-wide">
               Hola 👋!
               <br />
               Soy Chaplin, la IA de{" "}
-              <span className="text-accent font-semibold">QuePinta</span>.{" "}
+              <span className="text-accent text-sm md:text-base font-semibold">QuePinta</span>.{" "}
               <br /> <br /> Mi trabajo es ayudarte a encontrar la película o
               serie que estás buscando! <br /> <br /> Ten en cuenta que mientras
               más preciso (o precisa) seas, mejores serán mis recomendaciones
